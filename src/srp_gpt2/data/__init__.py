444941343fb20d7d9ccad9658419ceb47a105846
celsowm <celsowm@gmail.com>
2026-04-24 22:04:44 -0300

    Require Parquet datasets for training

--- src/srp_gpt2/data/__init__.py
+++ src/srp_gpt2/data/__init__.py
@@ -1,11 +1,10 @@
 """Data loading and tokenization utilities."""
 
-from srp_gpt2.data.dataset import HuggingFaceTextDataset, TextFileDataset
+from srp_gpt2.data.dataset import ParquetTextDataset
 from srp_gpt2.data.tokenizer import ByteTokenizer, GPT2BPETokenizer, TokenizerProtocol, build_tokenizer
 
 __all__ = [
-    "TextFileDataset",
-    "HuggingFaceTextDataset",
+    "ParquetTextDataset",
     "ByteTokenizer",
     "GPT2BPETokenizer",
     "TokenizerProtocol",
